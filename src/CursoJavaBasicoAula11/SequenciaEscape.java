99c1b07806fed1697d1460467f0cc4272d2b29b3
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-08-12 16:06:03 -0300

    CURSO DE JAVA AULA 11: TIPOS PRIMITIVOS (VERDADEIRO/FALSO E NÚMEROS INTEIRO E COM VÍRGULA\VÍRGULA + LETRA)

---
+++ src/CursoJavaBasicoAula11/SequenciaEscape.java
@@ -0,0 +1,16 @@
+package CursoJavaBasicoAula11;
+
+public class SequenciaEscape {
+
+	public static void main(String[] args) {
+		
+		
+		//"Hello, World!"
+		
+		System.out.print("\"Hello, World!\"\n\r");
+		// 1\4
+		System.out.println("1\\4");
+
+	}
+
+}
