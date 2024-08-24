d922f76e4dba0cd52162ff91c5bb6db63d9b5e5c
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-08-23 21:35:09 -0300

    CURSO DE JAVA - CORREÇÃO DE EXERCÍCIOS AULA 15 (IF-ELSE e SWITCH-CASE) PARTE 4

--- src/CursoJavaBasicoAula15Labs/Exec07.java
+++ src/CursoJavaBasicoAula15Labs/Exec07.java
@@ -2,26 +2,34 @@ package CursoJavaBasicoAula15Labs;
 
 import java.util.Scanner;
 
-public class Exer06 {
+public class Exec07 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Entre com o primeiro número");
 		int num1 = scan.nextInt();
-		
+
 		System.out.println("Entre com o segundo número");
 		int num2 = scan.nextInt();
-		
+
 		System.out.println("Entre com o terceiro número");
 		int num3 = scan.nextInt();
-		
-		if (num1 >= num2 && num1 >= num3 ){
-		    System.out.println("num1 é maior: " + num1);
-		} else if (num2 >= num1 && num2 >= num3 ){
+
+		if (num1 >= num2 && num1 >= num3) {
+			System.out.println("num1 é maior: " + num1);
+		} else if (num2 >= num1 && num2 >= num3) {
 			System.out.println("num1 é maior: " + num2);
-		} else if (num3 >= num1 && num3 >= num3 ){
+		} else if (num3 >= num1 && num3 >= num3) {
 			System.out.println("num1 é maior: " + num3);
+		}
+		
+		if (num1 <= num2 && num1 <= num3) {
+			System.out.println("num1 é menor: " + num1);
+		} else if (num2 <= num1 && num2 <= num3) {
+			System.out.println("num1 é menor: " + num2);
+		} else if (num3 <= num1 && num3 <= num3) {
+			System.out.println("num1 é menor: " + num3);
 		}
 
 	}
