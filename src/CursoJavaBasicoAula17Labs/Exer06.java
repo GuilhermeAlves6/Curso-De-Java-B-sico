4bbc82b3af1c142080f8278f41ac47893f4b9ca1
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-09-16 18:26:54 -0300

    CURSO DE JAVA - CORREÇÃO DE EXERCÍCIOS AULA 17 (WHILE,DO-WHILE,FOR) PARTE 4

---
+++ src/CursoJavaBasicoAula17Labs/Exer06.java
@@ -0,0 +1,17 @@
+package CursoJavaBasicoAula17Labs;
+
+public class Exer06 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for (int i=1; i<=20; i++) {
+			//System.out.println(i);
+			System.out.print(i + " "); //print é mesma linha - println é uma linha debaixo da outra
+		}
+	
+
+	}
+
+}
