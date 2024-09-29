3bf219a61998abeae9eff02b536ca8126827654b
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-09-28 21:52:07 -0300

    PARTE 12

---
+++ src/CursoJavaBasicoAula17Labs/Exer19.java
@@ -0,0 +1,33 @@
+package CursoJavaBasicoAula17Labs;
+
+import java.util.Scanner;
+
+public class Exer19 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o número de notas:");
+		int notas = scan.nextInt();
+		
+		int soma =0;
+		int media;
+		int nota;
+		
+		for (int i=0; i<notas; i++) {
+			System.out.println("Entre com a nota " + (i+1));
+			nota = scan.nextInt();
+			
+			
+			soma += nota;
+			
+			
+		}
+		
+		media = soma / notas;
+		
+		System.out.println("Soma " + soma);
+		System.out.println("Média " + media);
+	}
+
+}
