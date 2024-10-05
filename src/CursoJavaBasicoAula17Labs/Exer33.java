129432d2065e6936fd8be5fce647b86d7d3d4f76
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-10-05 06:24:17 -0300

    CURSO DE JAVA - CORREÇÃO DE EXERCÍCIOS AULA 17 (WHILE,DO-WHILE,FOR) PARTE 17

---
+++ src/CursoJavaBasicoAula17Labs/Exer33.java
@@ -0,0 +1,23 @@
+package CursoJavaBasicoAula17Labs;
+
+import java.util.Scanner;
+
+public class Exer33 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Dígite o valor de n:");
+		int n = scan.nextInt();
+		double soma = 0;
+
+		for (int i = 1, j = 1; i <= n; i++, j += 2) {
+
+			System.out.print(i + "/" + j + " + ");
+			soma += i/j;
+		}
+		
+		System.out.println("\nSoma = " + soma);
+	}
+}
---
+++ src/CursoJavaBasicoAula17Labs/Exer34.java
@@ -0,0 +1,27 @@
+package CursoJavaBasicoAula17Labs;
+
+import java.util.Scanner;
+
+public class Exer34 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan =  new Scanner(System.in);
+		
+		System.out.println("Dígite o valor de n:");
+		int n = scan.nextInt();
+		
+		double soma = 0;
+		
+		for (int i=1; i<=n; i++) {
+			
+			soma += 1/i;
+			
+		}
+		
+		System.out.println("Soma = " + soma);
+
+	}
+
+}
