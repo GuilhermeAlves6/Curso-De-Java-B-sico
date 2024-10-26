48e25ec33d527a78a9c8a45a939706171b5d88d8
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-10-25 23:25:41 -0300

    Matrizes

--- src/CursoJavaBasicoAula20/Matrizes.java
+++ src/CursoJavaBasicoAula20/Matrizes.java
@@ -4,7 +4,7 @@ public class Matrizes {
 
 	public static void main(String[] args) {
 		
-		double[][] notasAlunos = new double [30][4];
+		double[][] notasAlunos = new double [3][4];
 		
 		notasAlunos[0][0] = 10;
 		notasAlunos[0][1] = 7;
@@ -20,8 +20,10 @@ public class Matrizes {
 		notasAlunos[2][1] = 9;
 		notasAlunos[2][2] = 10;
 		notasAlunos[2][3] = 7;
-	
-				
+		
+		for (int i=0; i<notasAlunos.length; i++) {
+			System.out.print(notasAlunos[i] + " ");
+		}
 		
 
 	}
