f336f4a6314e6a139ffccb2eb821fc2618943f3b
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-10-26 10:39:31 -0300

    Curso de Java 20 Matrizes (Arrays Multidimensionais) - Manipulação - Parte 2

--- src/CursoJavaBasicoAula20/Matrizes.java
+++ src/CursoJavaBasicoAula20/Matrizes.java
@@ -22,9 +22,50 @@ public class Matrizes {
 		notasAlunos[2][3] = 7;
 		
 		for (int i=0; i<notasAlunos.length; i++) {
-			System.out.print(notasAlunos[i] + " ");
+			//System.out.print(notasAlunos[i] + " ");
+			for(int j=0; j<notasAlunos[i].length; j++){
+				System.out.print(notasAlunos[i][j] + " - ");
+			}
+			System.out.println();
 		}
 		
+		notasAlunos[1][3]= 8;
+		
+		System.out.println();
+		
+		for (int i=0; i<notasAlunos.length; i++) {
+			for(int j=0; j<notasAlunos[i].length; j++){
+				System.out.print(notasAlunos[i][j] + " - ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Calculando a média de cada aluno");
+		
+		double soma;
+		for (int i=0; i<notasAlunos.length; i++) {
+			
+			soma = 0;
+			for(int j=0; j<notasAlunos[i].length; j++){
+				soma+= notasAlunos[i][j];
+			}
+			System.out.println("Média do aluno " + i + " é " +(soma/4));
+		}
+		
+		double[]notasAluno1 = {7,8,9,10};
+		
+		double[][]notasAlunos2 = {{7,8,9,10},{8, 6, 7, 10}};
+		
+		System.out.println("Output da matriz notasAlunos2");
+		
+		for (int i=0; i<notasAlunos2.length; i++) {
+			for(int j=0; j<notasAlunos2[i].length; j++){
+				System.out.print(notasAlunos2[i][j] + " - ");
+			}
+			
+			System.out.println();
+			
+		}
 
 	}
 
