64bc2ef56df313efeb83c2c8f048b60b6aa09e5e
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-11-25 20:39:26 -0300

    Curso De Java - Orientação A Objeto: Correção Exercícios Aula 27: Classes e Métodos Parte 3

---
+++ src/CursoJavaBasicoAula27/labs/Exer03.java
@@ -0,0 +1,47 @@
+package CursoJavaBasicoAula27.labs;
+
+import java.util.Scanner;
+
+public class Exer03 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		Aluno aluno = new Aluno();
+
+		System.out.println("Entre com o nome do aluno:");
+		aluno.nome = scan.next();
+
+		System.out.println("Entre com o nome do curso:");
+		aluno.nomeCurso = scan.next();
+
+		System.out.println("Entre com a matrícula");
+		aluno.matricula = scan.next();
+
+		// aluno.nomeDisciplinas = new String[3];
+		for (int i = 0; i < aluno.nomeDisciplinas.length; i++) {
+			System.out.println("Entre com o nome da disciplina " + i);
+			aluno.nomeDisciplinas[i] = scan.next();
+
+		}
+
+		for (int i = 0; i < aluno.notasDisciplinas.length; i++) {
+			System.out.println("Obtendo notas da disciplina " + aluno.nomeDisciplinas[i]);
+			for (int j = 0; j < aluno.notasDisciplinas[i].length; j++) {
+				System.out.println("Entre com a nota " + (j + i));
+				aluno.notasDisciplinas[i][j] = scan.nextDouble();
+			}
+		}
+
+		aluno.mostrarInfo();
+		
+		for (int i = 0; i < aluno.nomeDisciplinas.length; i++) {
+			if (aluno.verificarAprovado(i)) {
+				System.out.println("Disciplina " + aluno.nomeDisciplinas[i] + " - Foi Aprovado");
+			} else {
+				System.out.println("Disciplina " + aluno.nomeDisciplinas[i] + " - Foi Reprovado");
+			}
+		}
+	}
+}
