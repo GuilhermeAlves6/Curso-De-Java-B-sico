124982550d51397fb7008d1d6e05ab8595c782fd
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-11-28 20:55:24 -0300

    Curso De Java 29 - Orientação A Objeto:Construtores

---
+++ src/CursoJavaBasicoAula28/Pacotes.java
@@ -0,0 +1,23 @@
+package CursoJavaBasicoAula28;
+
+
+import CursoJavaBasicoAula27.Carro;
+
+import CursoJavaBasicoAula20Labs.Exer04;
+import CursoJavaBasicoAula20Labs.Exer05;
+import CursoJavaBasicoAula20Labs.Exer06;
+
+public class Pacotes {
+	
+	public static void main(String[] args) {
+		
+		Carro carro;
+		
+		Carro carro2;
+		
+		Exer06 exer06;
+		Exer05 exer05;
+		Exer04 exer04;
+	}
+
+}
