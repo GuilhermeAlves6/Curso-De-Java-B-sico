7c2b20fece4eb74237d628e77d663db7c55c4656
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-11-29 21:11:59 -0300

    Curso De Java 31 - Orientação A Objetos: Modificadores Private e Public

---
+++ src/CursoJavaBasicoAula31alt/TesteCarroAlt.java
@@ -0,0 +1,16 @@
+package CursoJavaBasicoAula31alt;
+
+import CursoJavaBasicoAula31.Carro;
+
+public class TesteCarroAlt {
+	
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro();
+		carro.marca = "Fiat";
+		
+	
+		
+	}
+
+}
