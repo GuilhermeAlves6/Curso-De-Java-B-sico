15eec31ef1573521a32f8c4199e234c9462d67e5
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-12-10 23:23:09 -0300

    comentario

---
+++ src/CursoJavaBasicoAula36/labs/Aluno.java
@@ -0,0 +1,9 @@
+package CursoJavaBasicoAula36.labs;
+
+public class Aluno {
+	
+	private String nome;
+	private String matricula;
+	private double[] notas;
+
+}
---
+++ src/CursoJavaBasicoAula36/labs/Curso.java
@@ -0,0 +1,9 @@
+package CursoJavaBasicoAula36.labs;
+
+public class Curso {
+	
+	private String nome;
+	private String horario;
+	private Professor professor;//
+
+}
