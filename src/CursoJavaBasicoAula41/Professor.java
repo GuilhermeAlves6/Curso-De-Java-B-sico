85a338ff9b500b09ce4400f79ad0741eb83cf569
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-12-23 09:28:52 -0300

    Curso De Java 41: Orientação a Objetos: Herança:classes abstratas

---
+++ src/CursoJavaBasicoAula41/Professor.java
@@ -0,0 +1,41 @@
+package CursoJavaBasicoAula41;
+
+public class Professor extends Pessoa {
+	
+
+	private double salario;
+	private String nomeCurso;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public double calcularSalarioLiquido() {
+		return 0;
+	}
+	
+	public String obterEtiquetaEndereco() {
+
+		String s = "Endereço do Professor: ";
+		s += super.getEndereco();
+		
+		return s;
+	}
+	@Override
+	public void imprimirEtiquetaEndereco() {
+		
+		System.out.println("Imprimindo enderaço do Professor");
+		System.out.println(this.obterEtiquetaEndereco());
+		
+	}
+
+}
